b6d89154b61c821424c8cf1c91f6ac1f17e259f4
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2020-12-27 14:31:14 +0400

    init

---
+++ userspace-publication.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: UserSpace Publication
+  Plugin URI: https://userspace.com/
+  Description: Функционал публикаций на вашем сайте.
+  Version: 1.0.0
+  Author: Plechev Andrey
+  Author URI: https://codeseller.ru/
+  Text Domain: usp-publication
+  License: GPLv2 or later (license.txt)
+ */
+
